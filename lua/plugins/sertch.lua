dc7f2f4cb5103a30fc56612fd905d17cf6f59eff
hidehic0 <hidehico.ooba@gmail.com>
2024-11-01 18:03:39 +0900

    bufferline.nvimの設定を追加
    タブ移動のキーマップとタブを閉じるキーマップを追加

--- lua/plugins/sertch.lua
+++ lua/plugins/sertch.lua
@@ -11,6 +11,10 @@ return {
           require("telescope").load_extension("frecency")
         end,
       },
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+      },
     },
     cmd = { "Telescope" },
     keys = {
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -36,12 +36,16 @@ return {
           -- mode = "tabs",
           style_preset = bufferline.style_preset.minimal,
           always_show_bufferline = false,
-          show_buffer_close_icons = false,
-          show_close_icon = false,
           color_icons = true,
           diagnostics = "nvim_lsp",
         },
       })
+
+      vim.api.nvim_set_keymap("n", "gt", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+      -- 前のバッファへ移動
+      vim.api.nvim_set_keymap("n", "gT", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+      -- バッファを削除
+      vim.api.nvim_set_keymap("n", "<leader>q", ":bdelete<CR>", { noremap = true, silent = true })
     end,
   },
   {

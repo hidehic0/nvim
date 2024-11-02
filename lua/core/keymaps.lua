65445ee93b393e098935eb852d82e22df92e3cb0
hidehic0 <hidehico.ooba@gmail.com>
2024-11-02 15:21:58 +0900

    xでレジスタを汚さない設定を追加した

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,6 +1,9 @@
 -- mapの設定
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 -- jj をescにする設定
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
 -- クリップボードの設定
 vim.opt.clipboard:append({ "unnamedplus" })
+-- xでレジスタを汚さないようにする設定
+vim.api.nvim_set_keymap("n", "x", '"_x', { noremap = true, silent = true })

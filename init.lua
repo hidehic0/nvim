c2b17e77ecb33f3432fcf95d770a64de01d8325f
hidehic0 <hidehico.ooba@gmail.com>
2024-11-14 18:19:43 +0900

    ちょっと修正

--- init.lua
+++ init.lua
@@ -1,15 +1,3 @@
--- startup settings
-local cache_path = vim.fn.stdpath("cache") .. "/_defaults_cache.lua"
-local defaults_path = vim.fn.stdpath("config") .. "/lua/vim/_defaults.lua"
-
-if vim.fn.filereadable(cache_path) == 0 then
-  local f = io.open(cache_path, "w")
-  f:write(string.dump(loadfile(defaults_path)))
-  f:close()
-end
-
-dofile(cache_path)
-
 require("core/keymaps")
 require("core.options")
 require("lazyvim")

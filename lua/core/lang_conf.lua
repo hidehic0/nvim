d9fd7a8a4f696b71e3c97928ce90541825411aea
hidehic0 <hidehico.ooba@gmail.com>
2024-11-15 10:27:53 +0900

    .nixのインデント設定を追加

--- lua/core/lang_conf.lua
+++ lua/core/lang_conf.lua
@@ -48,3 +48,13 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.softtabstop = 2
   end,
 })
+
+-- nixのインデント設定
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "nix",
+  callback = function()
+    vim.opt_local.expandtab = true
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 2
+  end,
+})

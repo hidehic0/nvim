78f9be69dc933ddc909382701d563f2a951a3279
hidehic0 <hidehico.ooba@gmail.com>
2024-11-19 14:17:18 +0900

    lspsaga.nvimを使えるようにした

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -94,24 +94,24 @@ return {
     config = true,
   },
 
-  -- {
-  --   "nvimdev/lspsaga.nvim",
-  --   config = function()
-  --     require("lspsaga").setup({
-  --       diagnostic = {
-  --         -- 診断サインを無効にする
-  --         sign = false,
-  --       },
-  --       code_action = {
-  --         -- コードアクションのシンボルを非表示にする
-  --         show_server_name = false,
-  --       },
-  --     })
-  --   end,
-  --   dependencies = {
-  --     "nvim-treesitter/nvim-treesitter", -- optional
-  --     "nvim-tree/nvim-web-devicons", -- optional
-  --   },
-  --   event = "VimEnter",
-  -- },
+  {
+    "nvimdev/lspsaga.nvim",
+    config = function()
+      require("lspsaga").setup({
+        diagnostic = {
+          -- 診断サインを無効にする
+          sign = false,
+        },
+        code_action = {
+          -- コードアクションのシンボルを非表示にする
+          show_server_name = false,
+        },
+      })
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons",     -- optional
+    },
+    event = "LspAttach",
+  },
 }

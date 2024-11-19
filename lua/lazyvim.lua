c96c9510de7fae019ba1d3373a73d28ed010ebe6
hidehic0 <hidehico.ooba@gmail.com>
2024-11-19 14:29:32 +0900

    #1 インポート用の設定を変更

--- lua/lazyvim.lua
+++ lua/lazyvim.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -28,4 +28,6 @@ local opts = {
   },
 }
 
-require("lazy").setup("plugins", opts)
+require("lazy").setup({
+  { import = "plugins" },
+}, opts)

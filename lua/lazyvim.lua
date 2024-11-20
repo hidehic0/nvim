670e23aae315e5dd666143140a639911195f014f
hidehic0 <hidehico.ooba@gmail.com>
2024-11-20 10:00:14 +0900

    目的完遂

--- lua/lazyvim.lua
+++ lua/lazyvim.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
+      { out, "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -29,7 +29,7 @@ local opts = {
 }
 
 require("lazy").setup({
-  { import = "plugins" },
   { import = "plugins.lsp" },
   { import = "plugins.utils" },
+  { import = "plugins.ui" },
 }, opts)
--- lua/plugins/hlslens.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "kevinhwang91/nvim-hlslens",
-    event = "VeryLazy",
-    config = function()
-      require("hlslens").setup()
-    end,
-  },
-}

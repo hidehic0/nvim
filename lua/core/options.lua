0ec0901b991e3e5af4dc7fff8ce1653b99ecac67
hidehic0 <hidehico.ooba@gmail.com>
2024-11-25 17:15:06 +0900

    大体ok

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,6 +4,6 @@
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.cmd([["set guifont='HackGen Console NF'"]])
-
+vim.opt.signcolumn = "yes"
 vim.cmd([[set shadafile="NONE"]])
 vim.opt.termguicolors = true
--- lua/plugins/utils/ddu.lua
+++ lua/plugins/utils/ddu.lua
@@ -5,11 +5,31 @@ return {
     "Shougo/ddu-filter-matcher_substring",
     "Shougo/ddu-source-file_rec",
     "Shougo/ddu-ui-ff",
+    "Shougo/ddu-kind-file",
+    "uga-rosa/ddu-filter-converter_devicon",
   },
 
   config = function()
-    vim.fn["ddu#custom#patch_local"]("file_recursive", {
+    vim.fn["ddu#custom#patch_global"]({
       ui = "ff",
+      uiParams = {
+        ff = {
+          startAutoAction = true,
+          autoAction = {
+            delay = 0,
+            name = "preview",
+          },
+          floatingBorder = "rounded",
+          previewFloating = true,
+          previewFloatingBorder = "rounded",
+          previewFloatingTitle = "Preview",
+          previewSplit = "horizontal",
+          prompt = "> ",
+          split = "floating",
+        },
+      },
+    })
+    vim.fn["ddu#custom#patch_local"]("file_recursive", {
       sources = {
         {
           name = { "file_rec" },
@@ -17,6 +37,10 @@ return {
             matchers = {
               "matcher_substring",
             },
+            converters = {
+              "converter_devicon",
+            },
+            ignoreCase = true,
           },
         },
       },
@@ -34,6 +58,7 @@ return {
         vim.keymap.set({ "n" }, "q", [[<Cmd>call ddu#ui#do_action("quit")<CR>]], opts)
         vim.keymap.set({ "n" }, "<Cr>", [[<Cmd>call ddu#ui#do_action("itemAction")<CR>]], opts)
         vim.keymap.set({ "n" }, "i", [[<Cmd>call ddu#ui#do_action("openFilterWindow")<CR>]], opts)
+        vim.keymap.set({ "n" }, "P", [[<Cmd>call ddu#ui#do_action("togglePreview")<CR>]], opts)
       end,
     })
   end,

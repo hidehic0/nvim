0e40f133274433e550cc4e90e712be5c82f33a20
hidehic0 <hidehico.ooba@gmail.com>
2024-11-26 21:06:24 +0900

    また今度色々やろう

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,6 @@
 -- vim.opt.cursorcolumn = true
 -- vim.opt.cursorline = true
-
+vim.opt.swapfile = false
 vim.opt.relativenumber = true
 vim.opt.number = true
 vim.cmd([["set guifont='HackGen Console NF'"]])

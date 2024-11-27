e94d84bb61b2d8117a02aeffcdc155b5ac4c8cbc
hidehic0 <hidehico.ooba@gmail.com>
2024-11-27 20:55:21 +0900

    pum.vimの導入完了

--- lua/plugins/lsp/ddc.lua
+++ lua/plugins/lsp/ddc.lua
@@ -9,18 +9,53 @@ return {
       "Shougo/ddc-source-lsp",
       -- "Shougo/ddc-sorter_rank",
       "Shougo/ddc-converter_remove_overlap",
+      {
+        "Shougo/pum.vim",
+        config = function()
+          -- insert keybindings
+          vim.cmd([[
+inoremap <silent><expr> <TAB>
+      \ pum#visible() ? '<Cmd>call pum#map#insert_relative(+1)<CR>' :
+      \ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
+      \ '<TAB>' : ddc#map#manual_complete()
+inoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+inoremap <C-e>   <Cmd>call pum#map#cancel()<CR>
+
+cnoremap <Tab>   <Cmd>call pum#map#insert_relative(+1)<CR>
+    cnoremap <S-Tab> <Cmd>call pum#map#insert_relative(-1)<CR>
+          ]])
+        end,
+      },
+      "Shougo/ddc-ui-pum",
+      "Shougo/ddc-source-cmdline",
     },
     event = "InsertEnter",
     config = function()
       local patch_global = vim.fn["ddc#custom#patch_global"]
-      patch_global("ui", { "native" })
+      patch_global("ui", { "pum" })
       patch_global("autoCompleteEvents", {
         "InsertEnter",
         "TextChangedI",
         "TextChangedP",
         "CmdlineChanged",
       })
+      patch_global("cmdlineSources", {
+        "cmdline",
+      })
       patch_global("sources", { "lsp" })
+      patch_global("cmdlineSources", {
+        [":"] = {
+          "cmdline",
+          "around",
+        },
+        ["/"] = {
+          "around",
+        },
+        ["?"] = {
+          "around",
+        },
+      })
+
       patch_global("sourceOptions", {
         _ = {
           matchers = { "matcher_fuzzy" },
@@ -33,15 +68,10 @@ return {
           forceCompletionPattern = "\\.\\w*|:\\w*|->\\w*",
           sorters = { "sorter_lsp-kind", "sorter_fuzzy" },
         },
+        cmdline = {
+          mark = "[CMD]",
+        },
       })
-      vim.cmd([[
-inoremap <expr> <TAB>
-	\ pumvisible() ? '<C-n>' :
-	\ (col('.') <= 1 <Bar><Bar> getline('.')[col('.') - 2] =~# '\s') ?
-	\ '<TAB>' : ddc#map#manual_complete()
-
-	inoremap <expr> <S-TAB>  pumvisible() ? '<C-p>' : '<C-h>'
-      ]])
       vim.fn["ddc#enable"]()
     end,
   },
